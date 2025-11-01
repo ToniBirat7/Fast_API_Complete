157480f9b57be23e2c4e245049c8b09b0fc225f6
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-01 23:44:52 +0545

    docs: app router

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class CreateUser(BaseModel):
+  name: str
+  age: int
+  is_active: bool
 
 app = FastAPI()
-
-@app.get("/home/{user_id}/")
-async def home(user_id : int) -> dict[str, str | int]:
-  return {"msg" : f"Welcome Home {user_id}"}
-
-@app.get("/query")
-async def home_query(query: str | None) -> dict[str, str | int]:
-  print(f"Query, {query}")
-  return {"msg" : f"Query {query}"}
 
 @app.get("/pq/{usr_id}")
 async def path_query(usr_id: int,query: str | None) -> dict[str, str | int]:
   print(f"Query, {query}")
   print(f"Usr Id, {usr_id}")
   return {"msg" : f"Query {query}, Id {usr_id}"}
+
+@app.post("/user/")
+async def save_user(user : CreateUser) -> dict[str, CreateUser]:
+  return {"data" : user}
