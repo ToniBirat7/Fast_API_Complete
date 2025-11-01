6b78612f3ef6bf18f3f98eb8c20bb7b6be91b2bc
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-01 23:26:33 +0545

    docs: app router

--- app/main.py
+++ app/main.py
@@ -3,6 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/home/{user_id}/")
-async def home(user_id : int):
-  print(f"Hi, {user_id}")
+async def home(user_id : int) -> dict[str, str | int]:
   return {"msg" : f"Welcome Home {user_id}"}
+
+@app.get("/home/")
+async def home_query(query: str) -> dict[str, str | int]:
+  print(f"Query, {query}")
+  return {"msg" : f"Query {query}"}
