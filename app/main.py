e2b99611c253c7bc0f7ad82c1bbfd26068af98f2
ToniBirat7 <gotofreelancing1@gmail.com>
2025-11-01 23:34:17 +0545

    docs: app router

--- app/main.py
+++ app/main.py
@@ -6,7 +6,13 @@ app = FastAPI()
 async def home(user_id : int) -> dict[str, str | int]:
   return {"msg" : f"Welcome Home {user_id}"}
 
-@app.get("/home/")
-async def home_query(query: str) -> dict[str, str | int]:
+@app.get("/query")
+async def home_query(query: str | None) -> dict[str, str | int]:
   print(f"Query, {query}")
   return {"msg" : f"Query {query}"}
+
+@app.get("/pq/{usr_id}")
+async def path_query(usr_id: int,query: str | None) -> dict[str, str | int]:
+  print(f"Query, {query}")
+  print(f"Usr Id, {usr_id}")
+  return {"msg" : f"Query {query}, Id {usr_id}"}
